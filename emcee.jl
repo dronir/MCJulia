bed262f986722ff546f278519b2c8c9012642425
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-26 18:29:56 +0300

    First version of sampling
    Also added reset(S) and wrote get_lnprob() to actually do stuff

--- emcee.jl
+++ emcee.jl
@@ -43,51 +43,69 @@ function Sampler(n_walkers::Integer, dim::Integer, probfn::Function)
 end
 
 # Return lnprobs and blobs at given position
-function get_lnprob(S::Sampler, p::Array{Float64,2})
-	return 0,0
+function get_lnprob(S::Sampler, pos::Array{Float64,2})
+	lnprob = zeros(Float64, S.n_walkers)
+#	blobs = Array(Blob, S.n_walkers)
+#	b = false
+	for k = 1:S.n_walkers
+		p = S.probfn(vec(pos[k,:]))
+		if length(p) == 1
+			lnprob[k] = p
+		elseif length(p) == 2
+			b = true
+			lnprob[k] = p[1]
+			blobs[k] = p[2]
+		else
+			error("something weird came out of S.lnprobf()")
+		end
+	end
+	# FIXME
+	return lnprob, None
 end
 
 
 function sample(S::Sampler, p0::Array{Float64,2}, N::Int64, thin::Int64, storechain::Bool)
-	halfk = fld(S.n_walkers, 2)
+	k = S.n_walkers
+	halfk = fld(k, 2)
 
-	p = p0
+	p = copy(p0)
 	lnprob, blobs = get_lnprob(S, p)
+
+	i0 = size(S.chain, 3)
 
 	# Add N/thin columns of zeroes to the Sampler's chain and ln_posterior
 	if storechain
-		S.chain = hcat(S.chain, zeros(Float64, (S.n_walkers, S.dim, fld(N,thin))))
-		S.ln_posterior = hcat(S.ln_posterior, zeros(Float64, (S.n_walkers, fld(N,thin))))
+		S.chain = cat(3, S.chain, zeros(Float64, (k, S.dim, fld(N,thin))))
+		S.ln_posterior = cat(2, S.ln_posterior, zeros(Float64, (k, fld(N,thin))))
 	end
 
-	randZ() = randZ(S.a)
-
 	first = 1 : halfk
-	second = halfk+1 : S.n_walkers
+	second = halfk+1 : k
+	subsets = [(first, second), (second, first)]
 
 	# FIXME
 	#i0 = S.iteration
-	i0 = 0
+#	i0 = 0
 	for i = i0+1 : i0+N
-		for t in [(first, second), (second, first)]
+		for t in subsets
 			active, passive = t
-			l_act = length(active)
+#			l_act = length(active)
 			l_pas = length(passive)
 			for k in active
 				X0 = p[k,:]
 				choice = passive[randi(l_pas)]
 				X1 = p[choice,:]
-				z = randZ()
+				z = randZ(S.a)
 				Y = X1 + z * (X0 - X1)
-				new_lnprob = get_lnprob(S, Y)
-				q = (S.dim - 1) * log(z) + new_lnprob - lnprob
-				if q > log(rand())
-					lnprob = new_lnprob
+				new_lnprob = S.probfn(Y)
+				log_ratio = (S.dim - 1) * log(z) + new_lnprob - lnprob[k] # FIXME: lnprob
+				if log_ratio < log(rand())
+					lnprob[k] = new_lnprob
 					p[k,:] = Y
 				end
 				if (i-i0) % thin == 0
-					S.ln_posterior[k, i/thin] = lnprob
-					S.chain[k, :, i/thin] = p[k,:]
+					S.ln_posterior[k, i/thin] = lnprob[k]
+					S.chain[k, :, fld(i,thin)] = vec(p[k,:])
 				end
 			end
 		end
@@ -95,5 +113,11 @@ function sample(S::Sampler, p0::Array{Float64,2}, N::Int64, thin::Int64, storech
 end
 
 # Reset a Sampler to state after construction.
-function reset(Sampler)
+function reset(S::Sampler)
+	k = S.n_walkers
+	S.chain = zeros(Float64, (k, S.dim, 0))
+	S.ln_posterior = zeros(Float64, (k, 0))
+	S.blobs = Array(Blob, (k, 0))
+	S.acceptance_fraction = 0.0
+	return S
 end
