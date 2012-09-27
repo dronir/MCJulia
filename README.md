c679979beda51e26e1b348b9a03f935487f803db
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-27 17:53:38 +0300

    Add detailed comments to the test case.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 MC Julia is an affine-invariant ensemble MCMC sampler, written in the [Julia programming language](http://julialang.org). It implements a highly efficient MCMC algorithm by [Goodman & Weare (2010)](http://msp.org/camcos/2010/5-1/p04.xhtml), and is heavily inspired by the [`emcee`](http://danfm.ca/emcee/) Python package.
 
 The algorithm is very efficient at sampling probability distributions which are highly skewed but more or less linearly correlated.
+
+# Usage
+
+TODO: detailed instructions. In the meantime, look at the `test.jl` file for a simple example.
--- test.jl
+++ test.jl
@@ -1,17 +1,37 @@
+# A Simple MC Julia test case with detailed explanation
+
+# Load the module and import its public names
 load("mcjulia.jl")
 import MCJulia.*
 
-f(X) = -(X[1]-1.0)^2
+# In this simple test case we'll estimate the one-dimensional
+# probability distribution N(1, 1). We need to give the logarithm
+# of a function proportional to the actual density.
+# The position is given to the function as a vector.
+log_probability(X) = -(X[1]-1.0)^2
 
+# Set up the sampler with minimal options. We'll use 100 walkers
+# and the dimension of our probability space is 1.
 walkers = 100
-S = Sampler(walkers, 1, f)
+S = Sampler(walkers, 1, log_probability)
+
+# Generate random starting positions for all walkers with a uniform
+# distribution in the [-5, 5] interval.
 p0 = rand((walkers,1)) * 10 - 5
 
+# Do a 20-step burn-in without saving the results.  Since we have
+# 100 walkers, we are throwing away 2000 samples. The return value
+# p is the position of the walkers at the last step.
 p = sample(S, p0, 20, 1, false)
 
-tic()
+# Now the actual sampling run.
+# Run the sampler for 100 steps using p as a starting position, 
+# generating a total of 10000 samples.
 sample(S, p, 100, 1, true)
-toc()
 
-flat = flat_chain(S)
+# Flatten and save the chain into a file.
 save_chain(S, "chain.txt")
+
+# Uncomment the following to automatically run the simple Python
+# plotting program:
+ run(`python plot_chains.py chain.txt`)
