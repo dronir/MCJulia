5dd759364097a63b699cba224c0737f4c5b5fc15
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-27 16:57:17 +0300

    Change test case to exp(-(x-1)^2)

--- test.jl
+++ test.jl
@@ -1,9 +1,15 @@
 load("emcee.jl")
 import MCJulia.*
-S = Sampler(8, 2, log_rosenbrock)
-println(S)
-p0 = rand((8,2)) * 2
-sample(S, p0, 1000, 1, true)
+
+f(X) = -(X[1]-1.0)^2
+
+walkers = 100
+S = Sampler(walkers, 1, f)
+p0 = rand((walkers,1)) * 10 - 5
+
+tic()
+sample(S, p0, 100, 1, true)
+toc()
+
 flat = flat_chain(S)
 save_chain(S, "chain.txt")
-println(flat)
