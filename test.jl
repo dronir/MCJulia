b6195fbc2f72cd8f82b6866bbf4af75be676ab09
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-27 17:17:51 +0300

    Add burn-in to test program

--- test.jl
+++ test.jl
@@ -7,8 +7,10 @@ walkers = 100
 S = Sampler(walkers, 1, f)
 p0 = rand((walkers,1)) * 10 - 5
 
+p = sample(S, p0, 20, 1, false)
+
 tic()
-sample(S, p0, 100, 1, true)
+sample(S, p, 100, 1, true)
 toc()
 
 flat = flat_chain(S)
