aafca3d7d9af1aaaab530ac61fdb761cce49995c
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-27 17:52:14 +0300

    Add titles to chain plotter

--- plot_chains.py
+++ plot_chains.py
@@ -7,8 +7,6 @@ from sys import argv, exit
 
 filename = argv[1]
 
-burn_in = 0
-
 chain = loadtxt(filename, delimiter=",")
 s = chain.shape
 if len(s) > 1:
@@ -24,16 +22,17 @@ for i in xrange(dim):
     sub = dim*100 + 10 + i+1
     plot.subplot(sub)
     if dim > 1:
-        plot.plot(chain[i,burn_in:], color="black")
+        plot.plot(chain[i,:], color="black")
     else:
-        plot.plot(chain[burn_in:], color="black")
+        plot.plot(chain[:], color="black")
     
     plot.figure(i+1)
+    plot.title("Parameter #%d" % (i+1))
     if dim > 1:
-        plot.hist(chain[i,burn_in:], 20, color="black")
+        plot.hist(chain[i,:], 20, color="black")
     else:
-        plot.hist(chain[burn_in:], 20, color="black")
+        plot.hist(chain[:], 20, color="black")
 
 plot.figure(0)
-plot.axvline(burn_in, color="red")
+plot.title("Flattened chains for all parameters")
 plot.show()
