64009debfe49d7490feae73097b8092156081fb1
Olli Wilkman <olli.wilkman@iki.fi>
2012-09-27 16:56:48 +0300

    Fux plot_chains.py to plot also 1-dimensional chains

--- plot_chains.py
+++ plot_chains.py
@@ -7,21 +7,32 @@ from sys import argv, exit
 
 filename = argv[1]
 
-burn_in = 400
+burn_in = 0
 
 chain = loadtxt(filename, delimiter=",")
 s = chain.shape
-k = s[1]
-dim = s[0]
+if len(s) > 1:
+    steps = s[1]
+    dim = s[0]
+else:
+    steps = s[0]
+    dim = 1
+
 
 for i in xrange(dim):
     plot.figure(0)
     sub = dim*100 + 10 + i+1
     plot.subplot(sub)
-    plot.plot(chain[i,burn_in:], color="black")
+    if dim > 1:
+        plot.plot(chain[i,burn_in:], color="black")
+    else:
+        plot.plot(chain[burn_in:], color="black")
     
     plot.figure(i+1)
-    plot.hist(chain[i,burn_in:], 20, color="black")
+    if dim > 1:
+        plot.hist(chain[i,burn_in:], 20, color="black")
+    else:
+        plot.hist(chain[burn_in:], 20, color="black")
 
 plot.figure(0)
 plot.axvline(burn_in, color="red")
